a00d1a2da553561eabf4b107077dea1def6fccfc
Wellington Rocha <c-s-w@hotmail.com>
2020-12-02 12:23:14 -0300

    send files

--- test/data/binance-data.py
+++ test/data/binance-data.py
@@ -3,8 +3,8 @@ import json
 
 MARKET = 'BTCUSDT'
 ASSET = 'USDT'
-API_KEY = 'nloqXskw8UQPIzEt6TvkZXIlac45y3TUa92Wjotj9WhLaoGTqhZmvAxS2M09ginV'
-API_SECRET = 'YyQVnIGVILPjkp4lBTEFipO83E80K6dYLaIZ3TCOI5oUmjXyfVwQ6tl9gcqP5Gt6'
+API_KEY = ''
+API_SECRET = ''
 client = Client(API_KEY, API_SECRET)
 ohlc = client.futures_klines(symbol=MARKET, interval=Client.KLINE_INTERVAL_30MINUTE, limit=1500)
 print(ohlc)
