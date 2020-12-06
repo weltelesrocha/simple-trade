789ccdd0ecce1c8b8a29fbea466753e3d22fe55b
Wellington Rocha <c-s-w@hotmail.com>
2020-12-05 21:05:59 -0300

    send files

--- sma_10_20.py
+++ sma_10_20.py
@@ -3,8 +3,8 @@ from strategy import StrategySMA1020
 
 simple_trade_handler = SimpleTradeHandler(
     SimpleTradeConfig(
-        api_key='zTli0qrCrzcxmmuT5zJNNt7n1LBDqbkmBF3oXFvGysfCqpYAB7E2o2ybgawYRdeu',
-        api_secret='eS63dbBsiJuo02eJgdjE4HaNw4fWuqTJ5K5pXJVtVUBDg9jCLEeB2WjLW1terSUX'
+        api_key='',
+        api_secret=''
     ),
     StrategySMA1020()
 )
